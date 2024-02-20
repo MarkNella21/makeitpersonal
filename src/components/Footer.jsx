297d2f702819868178029abdaee5eb4682e73763
MarkNella21 <nelladevelopment@yahoo.com>
2024-02-19 20:13:32 -0800

    "commiting changes to merge conflicts on sitenav"

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Footer = () => {
+
+    return(
+        <div className="container">
+            <p className="align-text-left">NellaDevelopment est. 2022</p>
+        </div>
+    )
+}
+
+export default Footer;
