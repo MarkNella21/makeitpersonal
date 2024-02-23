634076f87ba5a8447df2661429bb217979c1d074
MarkNella21 <nelladevelopment@yahoo.com>
2024-02-22 17:50:03 -0800

    "merge conflict to Footer component."

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,20 +1,11 @@
 import React from "react";
 
-<<<<<<< HEAD
 const Footer = () =>{
 
     return(
         <footer className="container">
         <p>&copy; NellaDevelopment 2023-2024</p>
       </footer>
-=======
-const Footer = () => {
-
-    return(
-        <div className="container">
-            <p className="align-text-left">NellaDevelopment est. 2022</p>
-        </div>
->>>>>>> origin
     )
 }
 
